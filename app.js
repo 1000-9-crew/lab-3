dd3457bee406aab5de9f9d1ecc601a4d4eddcb8f
flex7r <artemrussu31@gmail.com>
2025-04-28 15:09:26 +0300

    squash relations-JSON
    Co-Authored-By: blarneyyy <159807402+blarneyyy@users.noreply.github.com>
    Co-Authored-By: Liudmyla <93478680+brieflyl@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,37 +1,50 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const session = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/authRouter');
+var teacherRouter = require('./routes/teacherRouter');
+var studentRouter = require('./routes/studentRouter');
+var homeRouter = require('./routes/homeRouter');
+var fileRouter = require('./routes/fileRouter');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 } // 1 week
+  })
+)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use("/demo", fileRouter);
+app.use("/", homeRouter);
+app.use("/auth", authRouter);
+app.use("/student", studentRouter);
+app.use("/teacher", teacherRouter);
+app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }));
+
+
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
